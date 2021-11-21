06bb2fe0774cdcc7ef5c550d0719d31ca9c2ac42
tetyana <tetyana.chouteau@gmail.com>
2021-11-21 15:15:33 +0100

    Header.jss, import Header dans App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./Card";
-
+import Header from "./Header";
 function App() {
   return (
     <body>
-      <header>
-        <div class="line">
-          <h1>Le bon service</h1>
-          <p>Echange de Services entre particuliers</p>
-          <h1 class="logo">
-            <img src="textencouleur.JPG" alt="" />
-          </h1>
-          <form>
-            <input type="search" name="q" placeholder="Rechercher" />
-            <input type="submit" value="Lancer" />
-          </form>
-        </div>
-
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Loisir</a>
-            </li>
-            <li>
-              <a href="./index.html">Sport</a>
-            </li>
-            <li>
-              <a href="#">Covoiturage</a>
-            </li>
-            <li>
-              <a href="#">Autre</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header></Header>
       <main>
         <aside>
           <input type="text" />
