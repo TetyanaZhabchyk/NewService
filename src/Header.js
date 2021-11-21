da651097970f9d61252bb51114cefcdbde5411c6
tetyana <tetyana.chouteau@gmail.com>
2021-11-21 15:25:24 +0100

    Header corrections

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <div class="line">
+        <h1>Le bon service</h1>
+        <p>Echange de Services entre particuliers</p>
+        <h1 class="logo">
+          <img src="textencouleur.JPG" alt="" />
+        </h1>
+        <form>
+          <input type="search" name="q" placeholder="Rechercher" />
+          <input type="submit" value="Lancer" />
+        </form>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Loisir</a>
+          </li>
+          <li>
+            <a href="./index.html">Sport</a>
+          </li>
+          <li>
+            <a href="#">Covoiturage</a>
+          </li>
+          <li>
+            <a href="#">Autre</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
