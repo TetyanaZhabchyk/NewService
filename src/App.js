75552986596046c97505dcec01599c5493ac9123
tetyana <tetyana.chouteau@gmail.com>
2021-11-21 13:53:46 +0100

    New projet, Card.js, Favori.js, ref="" d' app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+        <div class="line">
+          <h1>Le bon service</h1>
+          <p>Echange de Services entre particuliers</p>
+          <h1 class="logo">
+            <img src="textencouleur.JPG" alt="" />
+          </h1>
+          <form>
+            <input type="search" name="q" placeholder="Rechercher" />
+            <input type="submit" value="Lancer" />
+          </form>
+        </div>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Loisir</a>
+            </li>
+            <li>
+              <a href="./index.html">Sport</a>
+            </li>
+            <li>
+              <a href="#">Covoiturage</a>
+            </li>
+            <li>
+              <a href="#">Autre</a>
+            </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <aside>
+          <input type="text" />
+          <button>Details</button>
+        </aside>
+        <article>
+          <Card></Card>
+          <h1>titre de l'article</h1>
+          <p></p>
+          <p></p>
+        </article>
+      </main>
+      <footer></footer>
+    </body>
   );
 }
 
---
+++ src/Favori.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import "./Favori.css"; ♡
+
+function Favori() {
+  return (
+    <div class="favori">
+      <button>3</button>
+    </div>
+  );
+}
+
+export default Favori;
