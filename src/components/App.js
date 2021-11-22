b9fa53e43fa680c9f9115bcca40e5621f02c7245
tetyana <tetyana.chouteau@gmail.com>
2021-11-22 15:36:00 +0100

    Fetch Cards.json sans affichage

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import Header from "./Header";
 import Footer from "./Footer";
 function App() {
+  //declanche renouvellement d'affichage
+  let [cards, setCards] = useState([]);
+
+  //1fois outilisation -import react
+  useEffect(() => {
+    //recupere json et fait promesse (donees =data)
+    fetch("http://localhost:3001/cards.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setCards(data.cards);
+
+        // POURQUOI en Boucle ?
+        console.log("fetch");
+      });
+    //tab de depandancevide (1fois execution) 1fois pour composant qu'on charge
+    //17mlsec ...2sec
+  }, []);
+  // input en bootstrap react ???
   return (
     <body>
       <Header></Header>
