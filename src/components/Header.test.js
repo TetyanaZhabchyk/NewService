177190be35c239f5e346d619d1bd583abcbd8654
tetyana <tetyana.chouteau@gmail.com>
2021-12-01 18:02:38 +0100

    2 Tests de Role  d'input searchbox et de form button

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -3,21 +3,23 @@ import Header from "./Header";
 
 describe("<Header />", () => {
   test("should render the title of the website", () => {
-    render(<Header></Header>);
+    render(<Header />);
     expect(screen.getByText(/Le bon service/i)).toBeInTheDocument();
   });
   test("should render the baseline", () => {
-    render(<Header></Header>);
+    render(<Header />);
     expect(
       screen.getByText(/Echange de Services entre particuliers/i)
     ).toBeInTheDocument();
   });
-  test("should render not input abstract role", () => {
-    render(<Header></Header>);
-    expect(screen.getByText(/Rechercher/i)).toBeInTheDocument();
+
+  test("should render the input research", () => {
+    render(<Header />);
+    expect(screen.getByRole("searchbox")).toBeInTheDocument();
   });
-  test("should render value", () => {
-    render(<form></form>);
-    expect(screen.getByValue("Lancer").toBeInTheDocument());
+
+  test("should render form and button Lancer", () => {
+    render(<Header />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
 });
