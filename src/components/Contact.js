b3e4748b8dfe529f0ec64cab6fad60e41226d12f
tetyana <tetyana.chouteau@gmail.com>
2021-12-02 15:48:45 +0100

    Contact.js <title> Contact.css vide

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Contact.css";
+function Contact() {
+  return (
+    <contact>
+      <head>
+        <title>CONTACT</title>
+      </head>
+    </contact>
+  );
+}
+export default Contact;
